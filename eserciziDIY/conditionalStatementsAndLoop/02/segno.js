15587e0b844ecaa8e06dcc0d514fa82a07aea8eb
xxxNdr <andrea1987andre@gmail.com>
2025-08-30 17:16:12 +0200

    es2 ottimizzato

--- eserciziDIY/conditionalStatementsAndLoop/02/segno.js
+++ eserciziDIY/conditionalStatementsAndLoop/02/segno.js
@@ -7,15 +7,20 @@ while ((input = prompt("inserisci un numero positivo o negativo"))) {
 
 addEventListener("DOMContentLoaded", function () {
   ris = "";
+  segmento = "";
   for (x = 0; x < dati.length; x++) {
     val = Number(dati[x]);
     if (val > 0) {
-      ris += `il segno di ${val} è +`;
+      segmento = `il segno di ${val} è +`;
     } else if (val < 0) {
-      ris += ` e il segno di ${val} è -`;
+      segmento = `il segno di ${val} è -`;
     }
-    if (x < dati.length - 2) {
-      ris += ", ";
+    if (x < dati.length -1) {
+      ris += segmento + ", ";
+    } if (x === dati.length -1) {
+        ris = ris.replace(/, $/, "") + " e " + segmento
+        // la regex racchiusa in Js è racchiusa tra /
+        // ordina di cercare (, ) e di sostituire con stringa vuota ("")
     }
   }
   document.getElementById("r").innerHTML = ris;
